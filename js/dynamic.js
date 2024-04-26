cce28218008f55ce840c1289154989c0a95cd490
Antoine Ansari <antoine.ansari@gmail.com>
2024-04-27 00:49:51 +0200

    feat(website): bootstrap website

---
+++ js/dynamic.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('.nav');
+const menu = document.querySelector('.menu');
+const arrow = document.querySelector("#scroll-arrow")
+const firstSection = document.querySelector('.section');
+
+menu.addEventListener('click', () => {
+  nav.classList.toggle('nav-active');
+});
+
+arrow.addEventListener('click', () => {
+  firstSection.scrollIntoView({ behavior: 'smooth' });
+});
