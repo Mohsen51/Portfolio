6794e30754da32c7ea206d01b06d49e3fded124e
Antoine Ansari <antoine.ansari@gmail.com>
2024-04-28 14:01:54 +0200

    style: lint code

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,12 +1,12 @@
-const nav = document.querySelector('.nav');
-const menu = document.querySelector('.menu');
-const arrow = document.querySelector("#scroll-arrow")
-const firstSection = document.querySelector('.section');
+const nav = document.querySelector(".nav");
+const menu = document.querySelector(".menu");
+const arrow = document.querySelector("#scroll-arrow");
+const firstSection = document.querySelector(".section");
 
-menu.addEventListener('click', () => {
-  nav.classList.toggle('nav-active');
+menu.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
 });
 
-arrow.addEventListener('click', () => {
-  firstSection.scrollIntoView({ behavior: 'smooth' });
+arrow.addEventListener("click", () => {
+  firstSection.scrollIntoView({ behavior: "smooth" });
 });
